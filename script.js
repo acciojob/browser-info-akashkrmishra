c8b237235c27108ebdff54706e0d1124f8506795
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-24 07:44:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+window.onload = function() {
+			var browserInfo = "You are using " + navigator.appName + " version " + navigator.appVersion;
+			document.getElementById("browser-info").innerHTML = browserInfo;
+		}
